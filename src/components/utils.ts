048818f6b8b23e8ecb7f069c139bc22c47d9119a
Barrior <barrior@qq.com>
2020-04-19 19:26:57 +0800

    refactor with ts

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,9 +1,53 @@
-import { piBy180 } from './consts'
+import { piBy180, regExp } from './consts'
 
-export function limitRandom(max: number, min: number): number {
+/**
+ * 包装原生 parseInt，确保输出十进制数值
+ */
+export function pInt(str: string) {
+  return parseInt(str, 10)
+}
+
+/**
+ * 包装原生 toFixed，确保输出数字而不是字符串
+ */
+export function toFixed(num: number | string, digits: number) {
+  return parseFloat(Number(num).toFixed(digits))
+}
+
+/**
+ * 移除字符串内所有空白，包括空格、空行、制表符
+ */
+export function trimAll(str: string) {
+  return str.replace(regExp.trimAll, '')
+}
+
+/**
+ * 角度转弧度
+ */
+export function degreesToRadians(degrees: number) {
+  return degrees * piBy180
+}
+
+/**
+ * 弧度转角度
+ */
+export function radiansToDegrees(radians: number) {
+  return radians / piBy180
+}
+
+/**
+ * 限制随机数的范围
+ */
+export function limitRandom(max: number, min: number) {
   return max === min ? max : Math.random() * (max - min) + min
 }
 
-export function degreesToRadians(degrees: number): number {
-  return degrees * piBy180
+/**
+ * 获取随机速度
+ */
+export function speedRandom(maxSpeed: number, minSpeed: number) {
+  return (
+    (limitRandom(maxSpeed, minSpeed) || maxSpeed) *
+    (Math.random() > 0.5 ? 1 : -1)
+  )
 }
