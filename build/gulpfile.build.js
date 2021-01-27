d3672fc4fda66d2aba6461d233e176ac3aa1dd61
Barrior <barrior@qq.com>
2021-01-27 16:47:14 +0800

    chore: update something

---
+++ build/gulpfile.build.js
@@ -0,0 +1,71 @@
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const concat = require('gulp-concat')
+const through = require('through2')
+const wrap = require('./gulp-wrap')
+
+const fs = require('fs')
+const pkg = require('../package.json')
+
+const VERSION = pkg.version
+const COPYRIGHT = `/**
+ * JParticles v${VERSION} (https://github.com/Barrior/JParticles)
+ * Copyright 2016-present Barrior <Barrior@qq.com>
+ * Licensed under the MIT (https://opensource.org/licenses/MIT)
+ */
+`
+
+const srcPath = '../src/'
+const distPath = '../dist/'
+const matchFile = /jparticles(\.all)?\.js/
+const excludedFiles = /(jparticles(\.all)?\.js|maps)\s/g
+
+gulp.task('compile', () => {
+  return gulp
+    .src(`${srcPath}*.js`)
+    .pipe(
+      babel({
+        presets: ['es2015', 'stage-0'],
+        plugins: ['transform-decorators-legacy'],
+      })
+    )
+    .pipe(wrap())
+    .pipe(gulp.dest(distPath))
+})
+
+gulp.task('packing', ['compile'], () => {
+  let files = fs.readdirSync(distPath)
+
+  files = files.join(' ').replace(excludedFiles, '')
+  files = ('jparticles.js ' + files).split(' ')
+  files = files.map((filename) => {
+    return distPath + filename
+  })
+
+  return gulp
+    .src(files)
+    .pipe(concat('jparticles.all.js'))
+    .pipe(gulp.dest(distPath))
+})
+
+// Build JParticles.
+gulp.task('build', ['packing'], () => {
+  return gulp
+    .src(`${distPath}*.js`)
+    .pipe(uglify())
+    .pipe(
+      through.obj((file, encoding, callback) => {
+        if (matchFile.test(file.path)) {
+          let content = file.contents.toString()
+          content = COPYRIGHT + content
+          file.contents = new Buffer(content)
+        }
+
+        callback(null, file)
+      })
+    )
+    .pipe(gulp.dest(distPath))
+})
+
+gulp.task('default', ['build'])
---
+++ build/release.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+print_error() {
+  printf "\e[31m${1} \e[0m"
+}
+
+print_info() {
+  printf "\e[34m${1} \e[0m"
+}
+
+echo "✨ $(print_info 'Publishing...')"
+
+readonly BRANCH_NAME=$(git symbolic-ref HEAD --short)
+
+if [[ BRANCH_NAME != "master" ]]
+  then
+    print_error "error"
+    echo "Publish command running only on the master branch, please checkout it."
+    exit 1
+fi
+
+yarn build
+
+yarn test
+
+if [[ $? != 0 ]]
+  then exit 1
+fi
+
+cat NPM_README.md > README.md
+
+yarn login
+
+yarn publish
+
+git checkout .
+
+print_info "✨ Publish done.\n"
